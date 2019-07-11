9b395724d9261f2a3b2c2200bf2c56efcf50d931
greysoong <song-.-yu@hotmail.com>
2019-07-11 17:33:34 +0800

    add iris

--- iris.py
+++ iris.py
@@ -9,10 +9,15 @@ des:
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 import pandas as pd
+import numpy as np
 import pandas.plotting
+import mglearn
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+
+
+
 iris_dataset = load_iris()
-
-
 '''
 print ("Keys of iris_dataset:\n{}".format(iris_dataset.keys()))
 print (iris_dataset['DESCR'][:193]+"\n...")
@@ -33,4 +38,19 @@ print ("X_test shape:{}".format(X_test.shape))
 print ("y_test shape:{}".format(y_test.shape))
 '''
 iris_dataframe = pd.DataFrame(X_train,columns=iris_dataset.feature_names)
-grr = pd
+
+#grr = pd.plotting.scatter_matrix(iris_dataframe,c=y_train,figsize=(15,15),marker='o',hist_kwds={'bins':20},s=60,alpha=.8,cmap=mglearn.cm3)
+
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train,y_train)
+X_new=np.array([[5,2.9,1,0.2]])
+print ("X_new.shape:{}".format(X_new.shape))
+
+prediction = knn.predict(X_new)
+print("Prediction:{}".format(prediction))
+print("Predicted target name:{}".format(iris_dataset['target_names'][prediction]))
+
+y_pred = knn.predict(X_test)
+print("Test set predictions:\n{}".format(y_pred))
+print("Test set score:{:.2f}".format(np.mean(y_pred==y_test)))
+print ("Test set score:{:.2f}".format(knn.score(X_test,y_test)))
